2672123a06f30267004b798fac5cb078cc675dd4
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-11-17 11:05:05 +0330

    add stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::BufRead;
+
+use std::io;
+mod validator;
+mod data;
 fn main() {
-  println!("Hello, world!");
+
+  guide_and_greeting();
+  let mut user_input = String::new();
+  io::stdin()
+    .read_line(&mut user_input)
+    .expect("read line faild");
+  match validator::user_input_validator(&user_input) {
+      Ok(_) => {},
+      Err(_) => {},
+  };
+}
+
+fn guide_and_greeting() {
+  println!("Sieve of Eratosthenes");
+  println!("Enter the range: (separated with '-') [e.g 1 - 60]:");
 }
