d755a4604ffeffb9f65412999232e7f050970668
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-11-18 11:20:37 +0330

    add rederer and fi loop issue

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,10 +1,60 @@
-use std::{fmt, time::Duration};
+use std::{
+  fmt,
+  time::Duration,
+  io:: { stdout }
+};
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+};
 
 pub struct UserInput {
   pub start: u32,
   /// end is included
   pub end: u32,
   pub delay: Duration,
+}
+
+pub struct Number {
+  pub is_prime: Option<bool>,
+  pub value: u32,
+}
+
+impl Number {
+  pub fn print(&self) {
+    match self.is_prime {
+      None => {
+
+      execute!(
+        stdout(),
+        SetForegroundColor(Color::White),
+        Print(format!("{}", self.value)),
+        ResetColor
+      ).unwrap();
+
+      } Some(is_prime) => {
+        if is_prime {
+
+        execute!(
+          stdout(),
+          SetForegroundColor(Color::Green),
+          Print(format!("{}", self.value)),
+          ResetColor
+        ).unwrap();
+
+        } else {
+          
+        execute!(
+          stdout(),
+          SetForegroundColor(Color::Red),
+          Print(format!("{}", self.value)),
+          ResetColor
+        ).unwrap();
+
+        }
+      }
+    }
+  }
 }
 
 pub enum SOEErrors {
--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,5 +1,81 @@
-use crate::data::UserInput;
+use crate::data::{ UserInput, Number };
+use std::thread;
 
 pub fn runner(user_input: UserInput) {
-  todo!();
+  let mut numbers = create_numbers(user_input.start,user_input.end);
+
+  for i in 0..numbers.len() {
+    let (current_number, other_numbers) =
+      numbers.split_one_mut(i);
+    if current_number.value == 2 {
+      current_number.is_prime = Some(true);
+      continue;
+    }
+    if current_number.value == 1 {
+      current_number.is_prime = Some(false);
+      continue;
+    }
+    for number in other_numbers {
+      if number.value % current_number.value == 0 {
+        number.is_prime = Some(false);
+      }
+    }
+  }
+  render(&numbers);
+}
+
+fn create_numbers(start: u32, end: u32) -> Vec<Number> {
+  let mut result: Vec<Number> = Vec::with_capacity((end - start) as usize);
+  for i in start..end + 1 {
+    result.push(Number{
+      is_prime: None,
+      value: i,
+    })
+  }
+  return result;
+}
+
+
+fn render(numbers: &[Number]) {
+  let mut element_counter = 0;
+  for number in numbers {
+    number.print();
+    print!("\t");
+    element_counter += 1;
+    if element_counter % 6 == 0 { println!(""); }
+  }
+}
+
+// slice mut
+
+type ImplIteratorMut<'a, Item> =
+    ::std::iter::Chain<
+        ::std::slice::IterMut<'a, Item>,
+        ::std::slice::IterMut<'a, Item>,
+    >
+;
+trait SplitOneMut {
+  type Item;
+
+  fn split_one_mut (
+      self: &'_ mut Self,
+      i: usize,
+  ) -> (&'_ mut Self::Item, ImplIteratorMut<'_, Self::Item>);
+}
+
+impl<T> SplitOneMut for [T] {
+  type Item = T;
+  
+  fn split_one_mut (
+      self: &'_ mut Self,
+      i: usize,
+  ) -> (&'_ mut Self::Item, ImplIteratorMut<'_, Self::Item>)
+  {
+      let (prev, current_and_end) = self.split_at_mut(i);
+      let (current, end) = current_and_end.split_at_mut(1);
+      (
+          &mut current[0],
+          prev.iter_mut().chain(end),
+      )
+  }
 }
